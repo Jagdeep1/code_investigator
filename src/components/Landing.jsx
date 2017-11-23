05760e3f3de62e753f42f9aa0935b0682487a35f
abhinesh007 <abhinesh.gour93@gmail.com>
2017-11-23 23:18:06 +0530

    changes for landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,5 @@
-
 import {Link, withRouter} from 'react-router';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Landing extends Component {
 
@@ -11,46 +10,52 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1><code>Code Investigator</code></h1>
-        </header>
+        <header></header>
         <h1>This is landing page!</h1>
-        <div>
-          <nav className="nav-bar">
-            <li className="nav-elements">
+        <div className="landing-wrapper">
+          <nav className="landing-nav-bar">
+            <li className="landing-nav-elements">
+              <a href="#">
+                <h3 className="m-0">Code Investigator</h3>
+              </a>
+            </li>
+            <li className="landing-nav-elements">
               <Link to="/">Home</Link>
             </li>
-            <li className="nav-elements">
+            <li className="landing-nav-elements">
               <Link to="/dashboard">Dashboard</Link>
             </li>
-            <li className="nav-elements">
+            <li className="landing-nav-elements">
               <Link to="/signup">Signup</Link>
             </li>
-            <li className="nav-elements">
+            <li className="landing-nav-elements">
               <Link to="/login">Login</Link>
             </li>
-            <li className="nav-elements">
+            <li className="landing-nav-elements">
               <Link to="/charts">Charts</Link>
             </li>
-            <li className="nav-elements">
+            <li className="landing-nav-elements">
               <Link to="/testcomponent">TestComponents</Link>
             </li>
-            <li className="nav-elements pull-right">
-              <Link
-                to="/codeupload"
-                className="active"
-                style={{
-                'margin': '0'
-              }}>CodeUpload</Link>
+            <li className="landing-nav-elements pull-right">
+              <Link to="/codeupload" className="active m-0">CodeUpload</Link>
             </li>
           </nav>
+          <div className="col-md-5">
+            <h1>Get intelligent insights about the code that you've written</h1>
+            <h3>Analyze you code, get it reviewed according to the styleguide followed in your project</h3>
+            <div className="col-md-6">
+              <Link className="btn btn-primary" to="/login">Login</Link>
+            </div>
+            <div className="col-md-6">
+            <Link to="/dashboard">OR CONTINUE AS A GUEST</Link>
+            </div>
+            <div className="col-md-12 m-t-5"><Link to="/signup">DON'T HAVE AN ACCOUNT? SIGNUP</Link></div>
+          </div>
         </div>
       </div>
     );
   }
 }
-
-
-
 
 export default withRouter(Landing);
